cc6c949e205e34a8611b92cbc042946c470e4c3c
Monett <monet9812@gmail.com>
2023-12-13 23:29:51 +0900

    exercise: conversion specifications

--- playground.c
+++ playground.c
@@ -3,7 +3,9 @@
 
 int main(void)
 {
-    printf("Hello world!\n");
-    printf("%d\n", TEST);
+    printf("%5.3d\n", 40);
+    printf("%10.3f\n", 839.21f);
+    printf("%10.3e\n", 839.21f);
+    printf("%-10g\n", 839.21f);
     return 0;
 }
