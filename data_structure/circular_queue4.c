587abbf437bf36153c93f86e3855a511952833e4
Monett <monet9812@gmail.com>
2023-12-15 19:34:00 +0900

    exercise: circular queue and stack

---
+++ data_structure/circular_queue4.c
@@ -0,0 +1,132 @@
+#include <stdio.h>
+
+#define MAX_SIZE (5)
+
+typedef struct
+{
+    int front;
+    int rear;
+    int data[MAX_SIZE];
+} CircularQueue;
+
+void initialize_queue(CircularQueue *queue);
+int is_empty(CircularQueue *queue);
+int is_full(CircularQueue *queue);
+void enqueue(CircularQueue *queue, int value);
+void dequeue(CircularQueue *queue);
+void print_queue(CircularQueue *queue);
+
+int main(void)
+{
+    CircularQueue queue;
+
+    initialize_queue(&queue);
+
+    print_queue(&queue);
+
+    enqueue(&queue, 1);
+    enqueue(&queue, 2);
+    enqueue(&queue, 3);
+
+    print_queue(&queue);
+
+    enqueue(&queue, 4);
+    enqueue(&queue, 5);
+    enqueue(&queue, 6);
+    print_queue(&queue);
+
+    dequeue(&queue);
+    dequeue(&queue);
+    dequeue(&queue);
+
+    print_queue(&queue);
+    dequeue(&queue);
+    dequeue(&queue);
+    dequeue(&queue);
+    print_queue(&queue);
+
+    enqueue(&queue, 4);
+    enqueue(&queue, 5);
+    enqueue(&queue, 6);
+    dequeue(&queue);
+    enqueue(&queue, 5);
+    print_queue(&queue);
+
+    return 0;
+}
+
+void initialize_queue(CircularQueue *queue)
+{
+    queue->front = -1;
+    queue->rear = -1;
+}
+
+int is_empty(CircularQueue *queue)
+{
+    return (queue->front == -1 && queue->rear == -1);
+}
+
+int is_full(CircularQueue *queue)
+{
+    return ((queue->rear + 1) % MAX_SIZE == queue->front);
+}
+
+void enqueue(CircularQueue *queue, int value)
+{
+    if (is_full(queue))
+    {
+        printf("Queue is full. Cannot enqueue.\n");
+        return;
+    }
+
+    if (is_empty(queue))
+    {
+        queue->front = 0;
+        queue->rear = 0;
+    }
+    else
+    {
+        queue->rear = (queue->rear + 1) % MAX_SIZE;
+    }
+
+    queue->data[queue->rear] = value;
+}
+
+void dequeue(CircularQueue *queue)
+{
+    if (is_empty(queue))
+    {
+        printf("Queue is empty. Cannot dequeue.\n");
+        return;
+    }
+
+    if (queue->front == queue->rear)
+    {
+        initialize_queue(queue);
+    }
+    else
+    {
+        queue->front = (queue->front + 1) % MAX_SIZE;
+    }
+}
+
+void print_queue(CircularQueue *queue)
+{
+    if (is_empty(queue))
+    {
+        printf("Queue is empty.\n");
+        return;
+    }
+
+    int i = queue->front;
+
+    printf("Queue : ");
+
+    do
+    {
+        printf("%d ", queue->data[i]);
+        i = (i + 1) % MAX_SIZE;
+    } while (i != (queue->rear + 1) % MAX_SIZE);
+
+    printf("\n");
+}
